e0c4a14cb34c3a23dc13906c6f557d855ec26fec
xidoWeb <erivery2@gmail.com>
2016-10-05 11:50:59 +0900

    jquery_002_input버튼문제 현재 미해결

--- step_05_js/js/src/step2_01.js
+++ step_05_js/js/src/step2_01.js
@@ -9,6 +9,30 @@ $(function() {
 	// 	$(this).animate({height: '-=15px'});
 	// });
 
-	$('div:nth-child(1)').css({color:'#333', borderRadius:'1rem'});
-	$('div').eq(2).css({color:'#333', borderRadius:'1rem'});
+	// $('div:nth-child(1)').css({color:'#333', borderRadius:'1rem'});
+	$('div:odd').css({color:'#f0f', borderRadius:'50%'});
+	$('div:even').css({color:'#333', borderRadius:'1rem'});
+	// $('div').first().css({backgroundColor:'#fc0'});
+	$('div:first').css({backgroundColor:'#fc0'});
+	var badWord = ['yellow', 'blue'];
+	$('div:contains("yellow")').css({display:'none'});
+
+	// #ck 체크되면, (클릭했다는 조건)
+	// #blue 사라지기
+
+	// $('#ck:checked').on('click', function(){
+	// 	$('#blue').css({display:'none'})
+	// });
+
+
+	$('#ck').on('click', check);
+	
+	function check(){
+		var blue = $('#blue');
+		if($('#ck:checked')){
+			blue.css({display:'none'});
+		}else{
+			blue.css({display:'block'});
+		}
+	}
 });
