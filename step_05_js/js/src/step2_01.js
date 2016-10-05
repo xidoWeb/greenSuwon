6d14dcb776b0c329702161c652abc445d97b669b
xidoWeb <erivery2@gmail.com>
2016-10-05 11:59:36 +0900

    show/hide

--- step_05_js/js/src/step2_01.js
+++ step_05_js/js/src/step2_01.js
@@ -10,29 +10,40 @@ $(function() {
 	// });
 
 	// $('div:nth-child(1)').css({color:'#333', borderRadius:'1rem'});
-	$('div:odd').css({color:'#f0f', borderRadius:'50%'});
-	$('div:even').css({color:'#333', borderRadius:'1rem'});
-	// $('div').first().css({backgroundColor:'#fc0'});
-	$('div:first').css({backgroundColor:'#fc0'});
-	var badWord = ['yellow', 'blue'];
-	$('div:contains("yellow")').css({display:'none'});
+	// $('div:odd').css({color:'#f0f', borderRadius:'50%'});
+	// $('div:even').css({color:'#333', borderRadius:'1rem'});
+	// // $('div').first().css({backgroundColor:'#fc0'});
+	// $('div:first').css({backgroundColor:'#fc0'});
+	// var badWord = ['yellow', 'blue'];
+	// $('div:contains("yellow")').css({display:'none'});
 
-	// #ck 체크되면, (클릭했다는 조건)
-	// #blue 사라지기
+	// // #ck 체크되면, (클릭했다는 조건)
+	// // #blue 사라지기
 
-	// $('#ck:checked').on('click', function(){
-	// 	$('#blue').css({display:'none'})
-	// });
+	// // $('#ck:checked').on('click', function(){
+	// // 	$('#blue').css({display:'none'})
+	// // });
 
 
-	$('#ck').on('click', check);
-	
-	function check(){
-		var blue = $('#blue');
-		if($('#ck:checked')){
-			blue.css({display:'none'});
-		}else{
-			blue.css({display:'block'});
-		}
-	}
+	// $('#ck').on('click', check);
+
+	// function check(){
+	// 	var blue = $('#blue');
+	// 	if($('#ck:checked')){
+	// 		blue.css({display:'none'});
+	// 	}else{
+	// 		blue.css({display:'block'});
+	// 	}
+	// }
+	$('#red').on('click', function(){
+		$(this).next().show();
+	});
+
+	$('#yellow').on('click', function(){
+		$(this).hide();
+	});
+
+	$('#green').on('click', function(){
+		$(this).prev().toggle();
+	});
 });
