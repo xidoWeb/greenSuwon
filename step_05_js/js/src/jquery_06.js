2dedd833c63e9f44c7ef54e567d21f5a55025bc5
xidoWeb <erivery2@gmail.com>
2016-10-07 11:17:19 +0900

    반응형웹 작업시 필요한 view(jquery)

--- step_05_js/js/src/jquery_06.js
+++ step_05_js/js/src/jquery_06.js
@@ -1,6 +1,8 @@
 // jquery_06.js
 
 (function($) {
+	/*
+	// h1값을 파악후, p값을 h1의 절반으로 만들기
 	$('h1').on('click', function(	) {	
 		var h_width = $(this).width(); 
 			console.log(	h_width );
@@ -9,5 +11,33 @@
 			                       'width':h_width / 2
 			                      }, 500);
 	});
+	*/
+
+	// console.log($(window.document).width()+'px');
+// 화면의 가로값이 변경될 경우 새로고침처리
+	var win_width = $(window).width();
+	$(window).on('resize',function() {
+		var resize_width = $(window).width();
+		if(win_width !== resize_width){
+			location.reload();
+		}
+	});
+// 화면의 뷰형태에 따라 이미지 변경
+	(function winView() {
+			var win_width = $(window).width();
+	
+			if(win_width < 500){
+				$('body').animate({'backgroundColor':'#0af'});
+			} else if(500 <= win_width &&  win_width < 1024){
+				$('body').animate({'backgroundColor':'#a0f'});
+			} else {
+				$('body').animate({'backgroundColor':'#cca'});
+			}
+		})();
+
+	//winView();
+
+
+
 })(this.jQuery);
 
