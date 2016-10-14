013a714152bf18485f2b9dab645ce4b722701647
xidoWeb <erivery2@gmail.com>
2016-10-14 09:59:45 +0900

    join page 만들기 및 gulp데이터올리기

---
+++ burnout_step/gulp_test/gulpfile.js
@@ -0,0 +1,10 @@
+// gulpfile.js
+
+// Modules 호출
+var gulp = require('gulp'); // Gulp 모듈 호출
+
+// Gulp.task() 를 사용해 기본(Default) 테스크를 정의
+gulp.task('default', function () {
+    // 콘솔 객체에 메시지를 기록(log)해 봅니다.
+    console.log('gulp default 일이 수행되었습니다!!!');
+});
