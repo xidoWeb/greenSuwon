faa545ce3596ac13746111560f74be71f588ad4b
xidoWeb <erivery2@gmail.com>
2016-10-20 13:45:49 +0900

    mp3 player 완성 및 첨부내용

--- step_05_js/js/src/audio.js
+++ step_05_js/js/src/audio.js
@@ -63,3 +63,19 @@ $('.next').on('click', function() {
 
 // console.log(music_list.length);
 
+
+
+
+
+
+/* 
+// mp3 player만들때 필요한 메소드 및 기능
+	.eq(): 순서를 할당(요소, jQuery)-태그의 성격으로 사용시에는 
+	.get(): 순서를 할당(요소, DOM)-오디오,비디오
+	.load(): multi 기능을 사용시 불러와서 준비(로딩)
+	.play(): multi 기능을 재생
+	.pause(): multi 기능을 일시정지
+
+	배열형식을 활용!!, 가사 or 제목등을표기시 객체형식(결론: json)
+*/
+
