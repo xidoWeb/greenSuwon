225174d3e7b8480bde221147702d5801e855a7d7
xidoWeb <erivery2@gmail.com>
2016-11-03 10:08:39 +0900

    브라우저 사이즈별 jQuery 작동 체크

--- step_08_web_3_rwd/js/src/mob.js
+++ step_08_web_3_rwd/js/src/mob.js
@@ -1,11 +1,39 @@
 // mob.js
 $(function() {
- var unb = $('.unb');
- var menu_btn = unb.find('button');
 
- menu_btn.on('click', function(e) {
- 	e.preventDefault();
+// resize 되었을 경우의 행동
+ var winWidth = $(window).width();
 
- 	unb.find('ul').toggle();
+ $(window).on('resize', function() {
+ 	var nowWidth = $(window).width();
+
+ 	if(winWidth !== nowWidth){	
+	 	location.reload();
+ 	}
+
  });
+
+
+
+// ------------------------------------------
+// 브라우저 상태 확인
+	var $width = $(window).width();
+
+		if($width < 768){
+		// mobile 에서의 작동 내용	
+		 var unb = $('.unb');
+		 var menu_btn = unb.find('button');
+
+		 menu_btn.on('click', function(e) {
+		 	e.preventDefault();
+		 	unb.find('ul').toggle();
+		 });
+
+		}else if( (768 <= $width) && ($width < 1024) ){
+
+		}else{
+
+		}
+
+
 });
