9d55dc160ffe8442aba3e239a3052be214a06830
xidoWeb <erivery2@gmail.com>
2016-11-07 11:10:40 +0900

    sliding_menu 만들기_ 초간단한 방법1

--- step_09_jquery_detail/js/src/sliding_menu_01.js
+++ step_09_jquery_detail/js/src/sliding_menu_01.js
@@ -1,1 +1,20 @@
 //  sliding_menu_01.js
+
+$(function() {
+
+	//menu_button 클릭시 사라지게!!!
+	$('.btn>button').on('click', function(e) {
+		e.preventDefault();
+		// var asideLeft = $('aside').css('left');
+
+		// if(asideLeft >= 0){
+		// 	$('aside').stop().animate({left:0}, 500);
+		// }else{
+		// 	$('aside').stop().animate({left:'-15rem'}, 500);
+		// }
+
+	$('aside').toggleClass('active');
+	
+	});
+
+});
