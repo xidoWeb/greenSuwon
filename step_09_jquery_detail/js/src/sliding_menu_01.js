47f373d115bac5a9786fa522156db9911e51c640
xidoWeb <erivery2@gmail.com>
2016-11-07 15:22:12 +0900

    main_banner_base_setting

--- step_09_jquery_detail/js/src/sliding_menu_01.js
+++ step_09_jquery_detail/js/src/sliding_menu_01.js
@@ -12,9 +12,26 @@ $(function() {
 		// }else{
 		// 	$('aside').stop().animate({left:'-15rem'}, 500);
 		// }
-
-	$('aside').toggleClass('active');
-	
+		$('aside').toggleClass('active');
 	});
 
+	$('#tab_menu').tabs();
+
+/* #adBanner height 설정 */
+	var h1_h = $('h1').height(),
+			h1_padding_t = $('h1').css('paddingTop');
+			h1_padding_b = $('h1').css('paddingBottom');
+			win_h = $(window).height();
+
+	// console.log(win_h);
+	// console.log( h1_h );
+	// console.log(parseInt(h1_padding_t) );
+	// console.log(parseInt(h1_padding_b) );
+	// console.log(win_h -( h1_h + parseInt(h1_padding_t) + parseInt(h1_padding_b) ));
+	var section_h = (win_h -( h1_h + parseInt(h1_padding_t) + parseInt(h1_padding_b) ));
+
+	$('#adBanner').height(section_h);
+
+// 
+	$('#adBanner').find('li').last().prependTo('#adBanner>ul');
 });
