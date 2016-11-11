2df9ba3c50ab3d890dddb84061f85f94220d1cfc
xidoWeb <erivery2@gmail.com>
2016-11-11 11:00:49 +0900

    이미지 상품설명 및 모달창

--- step_09_jquery_detail/js/src/product.js
+++ step_09_jquery_detail/js/src/product.js
@@ -2,7 +2,6 @@
 
 $(function() {
 	var modal           = $('#modal');
-	var modal_list      = $('.product').find('a');
 	var close_btn       = $('.close');
 	var timed           = 500;
 	var baseUrl         = "./img/cars/";
@@ -10,14 +9,38 @@ $(function() {
 	// 	{}
 	// ];
 	var product_nar     = [
-			{"name":"product_name_1", "img":"car_01_thum.jpg", "alt":"img1", "nar":"어쩌고 내용1"},
-			{"name":"product_name_2", "img":"car_02_thum.jpg", "alt":"img2", "nar":"어쩌고 내용2"},
-			{"name":"product_name_3", "img":"car_03_thum.jpg", "alt":"img3", "nar":"어쩌고 내용3"},
-			{"name":"product_name_4", "img":"car_04_thum.jpg", "alt":"img4", "nar":"어쩌고 내용4"},
-			{"name":"product_name_5", "img":"car_05_thum.jpg", "alt":"img5", "nar":"어쩌고 내용5"},
+			{"name":"product_name_1", "img":"car_01_thum.jpg", "alt":"img1", 
+			 "nar":"am aliquid non vero nam. Laudantium numquam, nihil eos ullam voluptatum iusto doloremque distinctio perspiciatis minima quasi, nisi natus soluta deserunt officia aspernatur odit id saepe provident dolorem laborum quae magnam. Autem praesentium veniam voluptatem explicabo ipsam ipsum, rerum nulla natus quas molestias nihil officiis pariatur harum eos minima!"},
+			{"name":"product_name_2", "img":"car_02_thum.jpg", "alt":"img2", 
+			 "nar":"a quasi, nisi natus soluta deserunt officia aspernatur odit id saepe provident dolorem laborum quae magnam. Autem praesentium veniam voluptatem explicabo ipsam ipsum, rerum nulla natus quas molestias nihil officiis pariatur harum eos minima!"},
+			{"name":"product_name_3", "img":"car_03_thum.jpg", "alt":"img3", 
+			 "nar":"loram Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium fugiat natus atque tempora modi asperiores nisi ipsa quaerat nemo veniam omnis officia rem provident magnam quae repellat quisquam, cum animi totam corporis, sunt minima quibusdam. Libero quia iusto saepe architecto blanditiis odio atque tempore enim, mollitia ratione, eveniet pariatur ducimus sit, quibusdam aliquid non vero nam. Laudantium numquam, nihil eos ullam voluptatum iusto doloremque distinctio perspiciatis minima quasi, nisi natus soluta deserunt officia aspernatur odit id saepe provident dolorem laborum quae magnam. Autem praesentium veniam voluptatem explicabo ipsam ipsum, rerum nulla natus quas molestias nihil officiis pariatur harum eos minima!"},
+			{"name":"product_name_4", "img":"car_04_thum.jpg", "alt":"img4", 
+			 "nar":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium fugiat natus atque tempora modi asperiores nisi ipsa quaerat nemo veniam omnis officia rem provident magnam quae repellat quisquam, cum animi totam corporis, sunt minima quibusdam. Libero quia iusto saepe architecto blanditiis odio atque tempore enim, mollitia ratione, eveniet pariatur ducimus sit, quibusdam aliquid non vero nam. Laudantium numquam, nihil eos ullam voluptatum iusto doloremque distinctio perspiciatis minima quasi, nisi natus soluta deserunt officia aspernatur odit id saepe provident dolorem laborum quae magnam. Autem praesentium veniam voluptatem explicabo ipsam ipsum, rerum nulla natus quas molestias nihil officiis pariatur harum eos minima!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium fugiat natus atque tempora modi asperiores nisi ipsa quaerat nemo veniam omnis officia rem provident magnam quae repellat quisquam, cum animi totam corporis, sunt minima quibusdam. Libero quia iusto saepe architecto blanditiis odio atque tempore enim, mollitia ratione, eveniet pariatur ducimus sit, quibusdam aliquid non vero nam. Laudantium numquam, nihil eos ullam voluptatum iusto doloremque distinctio perspiciatis minima quasi, nisi natus soluta deserunt officia aspernatur odit id saepe provident dolorem laborum quae magnam. Autem praesentium veniam voluptatem explicabo ipsam ipsum, rerum nulla natus quas molestias nihil officiis pariatur harum eos minima!"},
+			{"name":"product_name_5", "img":"car_05_thum.jpg", "alt":"img5", 
+			 "nar":"loram Lorem ipsum dolor sit amet, "},
+	];
+	var product_list 		= [
+			{"img":"car_01_thum.jpg", "alt": "img_nar_1"},
+			{"img":"car_02_thum.jpg", "alt": "img_nar_2"},
+			{"img":"car_03_thum.jpg", "alt": "img_nar_3"},
+			{"img":"car_04_thum.jpg", "alt": "img_nar_4"},
+			{"img":"car_05_thum.jpg", "alt": "img_nar_5"},
 	];
 
+	// baseUrl+product_list[?].img
+	// baseUrl+product_list[?].alt
+	for(var i = 0; i < product_list.length; i++){
+		var pr_li = $('.product').find('ul');
+		pr_li.append('<li><a href="#"><img>');
+		pr_li.find('li').eq(i).find('img').attr({src:baseUrl+product_list[i].img});
+	}
+		pr_li.find('img').css({height:'100%', marginLeft:"-20%"});
+		pr_li.find('li').css({overflow:'hidden'});
 
+
+	var modal_list      = $('.product').find('a');
+	
 	// modal 숨김처리
 	modal.hide();
 	modal_list.on('click', function(event) {
