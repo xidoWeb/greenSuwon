5b54daf91aa6b08809efe819f218edef8ace02a6
xidoWeb <erivery2@gmail.com>
2016-11-14 10:45:45 +0900

    scroll_top btn 포함 jquery내용

--- step_09_jquery_detail/js/src/scroll_page_01.js
+++ step_09_jquery_detail/js/src/scroll_page_01.js
@@ -1,6 +1,17 @@
 // scroll_page_01.js
 
 $(function() {
-	
-	
+
+	var page_link = $('#headBox a');
+	page_link.on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop:$(this.hash).offset().top});
+	});
+
+	var btn_top = $('#topBtn>button');
+	btn_top.on('click',function(e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop:0});
+	})
+
 })(this.jQuery);
