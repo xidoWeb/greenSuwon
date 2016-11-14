999f3ff536aacdd113880e7f48e174de8ccd7f81
xidoWeb <erivery2@gmail.com>
2016-11-14 11:55:55 +0900

    scroll위치에따른 화면 나타내기(parallax_01)

--- step_09_jquery_detail/js/src/scroll_page_01.js
+++ step_09_jquery_detail/js/src/scroll_page_01.js
@@ -14,14 +14,25 @@ $(function() {
 		$('html, body').animate({scrollTop:0});
 	});
 
-	var headbox = $('#headBox');
-	var head_offset = headbox.offset().top;
+	var headbox = $('#headBox'),
+	    head_offset = headbox.offset().top,
+	    conBox_offset = $('#conBox').offset().top,
+	    subBox_offset = $('#subBox').offset().top;
+
 	$(window).on('scroll', function() {
 		var scroll_top = $(window).scrollTop();
 		// headbox.addClass('fix');
 		headScroll(scroll_top);
 		// console.log(head_offset);
 		// console.log(scroll_top);
+		if(scroll_top >= conBox_offset){
+			$('#conBox').find('p').fadeIn(1000);
+		}	
+		if(scroll_top >= subBox_offset){
+			$('#subBox').find('img').fadeIn();
+		}
+
+
 	});
 
 	function headScroll(t) {
@@ -32,4 +43,6 @@ $(function() {
 				headbox.removeClass('fix');
 			}
 	};
+
+
 });
