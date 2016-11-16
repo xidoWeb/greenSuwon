53c7cc4a8b848d02bc1e271188a122f69e03c244
xidoWeb <erivery2@gmail.com>
2016-11-16 12:22:44 +0900

    일부 효과 변경

--- step_09_jquery_detail/js/src/parallax_01.js
+++ step_09_jquery_detail/js/src/parallax_01.js
@@ -1,15 +1,20 @@
 //  parallax_01.js
 
 $(function() {
+
+	$('.logo').css({transform:'scale(0)'});
+
 	$(window).on('scroll',function() {
 		// console.log( $(this).scrollTop()/10 );
 		var wScroll = $(this).scrollTop();
 
 		$('.logo').css({
-			transform:'translate(0px, '+ wScroll +'%)'
+			transform:'translate(0, '+ wScroll +'%) scale('+ wScroll/300 +')',
+			opacity:wScroll/100
+
 		});
 		$('.back_bird').css({
-			transform:'translate(0px, '+ wScroll/4 +'%)'
+			transform:'translate('+ wScroll/10+'%,'+ wScroll/4 +'%)'
 		});
 		$('.front_bird').css({
 			transform:'translate(0px, -'+ wScroll/20 +'%)'
