e06d2e333d4b22b87deadac047d5f9db8a1d4ac8
xidoWeb <erivery2@gmail.com>
2016-11-16 12:14:37 +0900

    parallax 스크롤에따른 위치변화 js코드

--- step_09_jquery_detail/js/src/parallax_01.js
+++ step_09_jquery_detail/js/src/parallax_01.js
@@ -1,1 +1,18 @@
 //  parallax_01.js
+
+$(function() {
+	$(window).on('scroll',function() {
+		// console.log( $(this).scrollTop()/10 );
+		var wScroll = $(this).scrollTop();
+
+		$('.logo').css({
+			transform:'translate(0px, '+ wScroll +'%)'
+		});
+		$('.back_bird').css({
+			transform:'translate(0px, '+ wScroll/4 +'%)'
+		});
+		$('.front_bird').css({
+			transform:'translate(0px, -'+ wScroll/20 +'%)'
+		});
+	});
+});
