c3d23fbd8015cbcaf2f15fb2774b1f4ee8718d38
xidoWeb <erivery2@gmail.com>
2016-11-28 13:43:26 +0900

    banner_auto slide

--- step_09_jquery_detail/js/src/slide_banner.js
+++ step_09_jquery_detail/js/src/slide_banner.js
@@ -1,19 +1,21 @@
 //  slide_banner.js
 $(function() {
-	var pHide = $('p').hide();
-	var timeOut = $('.set_out');
+/* //기초 내용 1------------------
+	var pHide = $('#addBanner').hide();
+	var timeOut = $('#addBanner');
 	var timeUp = function() {
 		timeOut.fadeIn();
 	};
 	// timeUp();
 	// setTimeOut(일정시간 뒤 실행할 기능, 일정시간);
-	// setTimeOut(timeUp, 3000);  // 일부수정!!
+	// setTimeout(timeUp, 3000); 
 
 	// setInterval(실행할 함수 , 일정 시간간격); 
 	// var log = function() {
 	// 	console.log('test');
 	// };
 	// setInterval(log, 2000);
+*/
 
 // ---------------------------------
 	var lbtn = $('.lbtn');
@@ -41,17 +43,20 @@ $(function() {
 		});
 		ol.find('li:first').appendTo(ol);
 	});
-	var setTime = 0;
+	var intval;
 	// setInterval 일정시간마다 움직임 처리
-	// var Intval = function() { 
-		setTime = setInterval( "$('.rbtn').click()" , 1500 );
-	// };
+	function Interval() { 
+		 intervalGo = setInterval( "$('.rbtn').click()" , 1500 );
+		return intervalGo;
+	};
 	// setInterval 의 기능을 초기화(삭제)
-	function ClearI() {clearInterval( setTime );};
+	function ClearI() {clearInterval( intervalGo );};
 
 // 일정시간마다 움직이도록 처리
-	// Intval();
-	box.on('mouseenter', ClearI);
+	// $('.banner_box').on('mouseenter', ClearI)
+	//                 .on('mouseleave', Interval);
+	box.on({'mouseenter':ClearI, 'mouseleave': Interval});
+	Interval();
 
 
 });
