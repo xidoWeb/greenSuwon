a066617c993801c7cd2b763e25d9b0f06a57226e
xidoWeb <erivery2@gmail.com>
2016-11-29 14:45:29 +0900

    자동 배너이동, 추가 기능 활성화

--- step_09_jquery_detail/js/src/indicator_slide.js
+++ step_09_jquery_detail/js/src/indicator_slide.js
@@ -10,10 +10,31 @@ var slideBanner = $('#slideBanner'), // 슬라이드 배너
 		bannerBoxLiWidth = bannerBox.find('li').width(slideBannerWidth+'px'),
 		bannerBoxWidthSet = slideBannerWidth * (bannerBoxLi+1),
 		delayTime = 100;
+		running = 2000;
+
+// banner갯수파악후 indicator 생성
+var slideIndiCator = slideBanner.append('<ol class="indicator">');
+	
 
 // indicator 변수 할당
 var indicator = $('.indicator');
+
+// 선택 인디케이터 class 기능추가
+var IndiClass = function() {
+	indicator.find('li').not('li:first').attr({style:null}).css({transition:'all 0.3s'});
+	$('.indiClass').css({width:'4rem',
+	                     borderRadius:'2rem',
+	                     transition:'all 0.3s',
+	                     backgroundColor:'#f4a'});
+};
+
+
+
 	// indicator는 배너의 갯수만큼 자동 생성되도록 처리!!
+for(var i=0; i<bannerBoxLi; i++ ){
+	indicator.append('<li><a href="#" role="button">');
+	// console.log(i);
+}
 
 // --------------------
 		bannerBox.width(bannerBoxWidthSet +'px'); // 배너의 박스 넓이
@@ -22,19 +43,29 @@ var indicator = $('.indicator');
 
 		// indicator 처음 복제된 버튼 숨기기, 1번 버튼색처리
 		indicator.find('li:first').hide();
-		indicator.find('li').eq(1).css({backgroundColor:'#f4a'});
-
+		// indicator.find('li').eq(1).css({backgroundColor:'#f4a'});
+		indicator.find('li').eq(1).addClass('indiClass');
+		IndiClass();
 // indicator버튼 클릭시 banner 이동하여 위치
 
 	indicator.find('li').on('click', function(e) {
 		e.preventDefault();
 		var $this = $(this),
         index = $this.index();
-
-    $this.css({backgroundColor:'#f4a'});
-    indicator.find('li').not($this).css({backgroundColor:'#fff'});
+    $this.addClass('indiClass');
+    indicator.find('li').not($this).removeClass('indiClass');
     bannerBox.stop().delay(delayTime).animate({marginLeft:-100 * index +'%'});
+    IndiClass();
 	});
+
+// slideBanner 버튼 클릭시 연속클릭에의한 오류를 방지하기위해 간단히 숨김처리
+slideBanner.find('button').on('click',function() {
+	var $this = $(this);
+		$this.stop().fadeOut(20, function() {
+			$this.delay(300).stop().fadeIn(20);
+		});
+
+});
 
 // $('lbtn') 클릭시 배너 이동
 var lbtn = $('.lbtn');
@@ -42,7 +73,7 @@ var lbtn = $('.lbtn');
 	lbtn.on('click', function(e) {
 		e.preventDefault();
 		var bannerBoxMargin = parseInt(bannerBox.css('marginLeft')),
-				bannerIndex = Math.abs(bannerBoxMargin / slideBannerWidth)-1;
+				bannerIndex = Math.abs(parseInt(bannerBoxMargin / slideBannerWidth))-1;
 				console.log(bannerIndex);
 /*
 		// banner_box의 마진값이 0일경우 마지막 위치로 이동
@@ -65,11 +96,12 @@ var lbtn = $('.lbtn');
 
 
 		// indicator navi 위치 찾아가기
-		if(bannerIndex == 0){
+		if(bannerIndex >= 0){
 			bannerIndex = bannerBoxLi;
 		}
-			indicator.find('li').css({backgroundColor:'#fff'});
-			indicator.find('li').eq(bannerIndex).css({backgroundColor:'#f4a'});
+			indicator.find('li').removeClass('indiClass');
+			indicator.find('li').eq(bannerIndex).addClass('indiClass');
+			IndiClass();
 	});
 
 // $('rbtn') 클릭시 배너 이동
@@ -77,10 +109,10 @@ var rbtn = $('.rbtn');
 	rbtn.on('click', function(e) {
 		e.preventDefault();
 		var bannerBoxMargin = parseInt(bannerBox.css('marginLeft')),
-				bannerIndex = Math.abs(bannerBoxMargin / slideBannerWidth)+1;
+				bannerIndex = Math.abs(parseInt(bannerBoxMargin / slideBannerWidth))+1;
 				console.log(bannerIndex);
 
-		if(bannerBoxMargin == (-slideBannerWidth*bannerBoxLi)){
+		if(bannerBoxMargin <= (-slideBannerWidth*bannerBoxLi)){
 			bannerBox.stop().css({marginLeft:0});
 			bannerBox.stop().delay(delayTime).animate({marginLeft:-slideBannerWidth});
 		}else{	
@@ -89,11 +121,24 @@ var rbtn = $('.rbtn');
 	if(bannerIndex == bannerBoxLi+1){
 			bannerIndex = 1;
 		}
-			indicator.find('li').css({backgroundColor:'#fff'});
-			indicator.find('li').eq(bannerIndex).css({backgroundColor:'#f4a'});
+			indicator.find('li').removeClass('indiClass');
+			indicator.find('li').eq(bannerIndex).addClass('indiClass');
+			IndiClass();
 	});
-
+// 배너영역 넘침 숨김처리
 slideBanner.css({overflow:'hidden'});
+
+// 자동 배너 움직임처리
+var startTime;
+var setTime = function() {
+		return startTime = setInterval("$('.rbtn').click()", running);
+};
+var clearTime = function() {clearInterval( startTime )};
+
+slideBanner.on({'mouseenter':clearTime, 'mouseleave':setTime});
+setTime();
+
+
 
 // end-----------------------------------------------
 });
